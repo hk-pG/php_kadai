ae6267004a223b68b50aceec935f598ddd77a61c
hk-p114514 <hukada.gogotea@gmail.com>
2022-05-02 00:45:39 +0900

    change logo images

--- js/src/main.ts
+++ js/src/main.ts
@@ -1,6 +1,7 @@
 import { insertDateElement } from './insertDateElement';
 const headerDate = document.getElementById('header-date');
 
+// print the date in the header
 if (headerDate) {
 	insertDateElement(headerDate);
 }
