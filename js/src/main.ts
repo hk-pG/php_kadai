0138404e716e99a6116c62430159fe52e67cc2e4
hk-p114514 <hukada.gogotea@gmail.com>
2022-05-13 15:41:40 +0900

    delete js directory

--- js/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { insertDateElement } from './insertDateElement';
-const headerDate = document.getElementById('header-date');
-
-// print the date in the header
-if (headerDate) {
-	insertDateElement(headerDate);
-}
